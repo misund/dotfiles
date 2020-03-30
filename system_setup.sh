478c8ea3c280b27f38457165f7c1af7622d4a070
misund <thomas@hemmeligadresse.com>
2020-03-30 19:25:22 +0200

    feature(system install): differentiate cmd tools and visual programs
    - prompt to ask if you want to install the visual programs
    - fix the check to see if nvm is already installed

--- system_setup.sh
+++ system_setup.sh
@@ -1,34 +1,47 @@
 # Install my favourite programs on a new and shiny system
 
+echo "Installing command line tools..."
+
 # Transfer data with URL syntax
-command -v curl >/dev/null 2>&1 || { sudo apt install curl; }
+command -v curl >/dev/null 2>&1 && echo "curl has already been installed on this system." || { sudo apt install curl; }
 
 # Version control
-command -v git >/dev/null 2>&1 || { sudo apt install git; }
+command -v git >/dev/null 2>&1 && echo "git has already been installed on this system." || { sudo apt install git; }
 
 # Text editor
-command -v vim >/dev/null 2>&1 || { sudo apt install vim; }
+command -v vim >/dev/null 2>&1 && echo "vim has already been installed on this system." || { sudo apt install vim; }
 
 # Terminal multiplexer
-command -v tmux >/dev/null 2>&1 || { sudo apt install tmux; }
-
-# Copy to clipboard from command line
-command -v xclip >/dev/null 2>&1 || { sudo apt install xclip; }
-
-# Browser
-command -v chromium >/dev/null 2>&1 || { sudo apt install chromium-browser; }
-
-# Integrated development environment (IDE)
-command -v atom >/dev/null 2>&1 || { wget -O atom.deb https://atom.io/download/deb && sudo dpkg --install atom.deb && rm atom.deb; }
-
-# Desktop manager
-command -v gdm3 >/dev/null 2>&1 || { sudo apt install gdm3; }
-
-# Node version manager
-command -v nvm >/dev/null 2>&1 || { curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | sh; source ~/.bash_profile; nvm install stable; nvm use stable; }
+command -v tmux >/dev/null 2>&1 && echo "tmux has already been installed on this system." || { sudo apt install tmux; }
 
 # Silver searcher
-command -v ag >/dev/null 2>&1 || { sudo apt install silversearcher-ag; }
+command -v ag >/dev/null 2>&1 && echo "ag has already been installed on this system." || { sudo apt install silversearcher-ag; }
+
+# Copy to clipboard from command line
+command -v xclip >/dev/null 2>&1 && echo "xclip has already been installed on this system." || { sudo apt install xclip; }
+
+# Node version manager
+[ -d "$NVM_DIR/.git" ] && echo "nvm has already been installed on this system." || { curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | sh; source ~/.bash_profile; nvm install stable; nvm use stable; }
+
+echo ""
+echo "Would you like to install visual stuffs? This includes heavy things like gnome, spotify, chromium and atom."
+read -p "Continue? [Y/n]:" OKGO
+OKGO=${OKGO:-Y}
+if [[ $OKGO =~ ^[yY]|[yY][eE][sS]$ ]]
+then
+	echo "You were warned. Commencing shininess..."
+else
+	exit
+fi
+
+# Browser
+command -v chromium >/dev/null 2>&1 && echo "chromium has already been installed on this system." || { sudo snap install chromium; }
+
+# Integrated development environment (IDE)
+command -v atom >/dev/null 2>&1 && echo "atom has already been installed on this system." || { wget -O atom.deb https://atom.io/download/deb && sudo dpkg --install atom.deb && rm atom.deb; }
+
+# Desktop manager
+command -v gnome-session >/dev/null 2>&1 && echo "gnome-session has already been installed on this system." || { sudo apt install gnome-session; }
 
 # Music player
-command -v spotify >/dev/null 2>&1 || { sudo snap install spotify; }
+command -v spotify >/dev/null 2>&1 && echo "spotify has already been installed on this system." || { sudo snap install spotify; }
