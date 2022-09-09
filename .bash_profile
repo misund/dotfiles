c256b44a74968a08cd4ec8593423fc6ece561d2b
misund <thomas@hemmeligadresse.com>
2022-09-09 16:54:52 +0200

    feat(system setup): Add many programs to install (including golang, code, cheese)

--- .bash_profile
+++ .bash_profile
@@ -4,5 +4,6 @@
 
 [[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh && nvm alias default stable # This loads NVM
 [[ -r $NVM_DIR/bash_completion ]] && . $NVM_DIR/bash_completion # Bash completion for NVM
+[[ -d /usr/local/go/bin ]] && export PATH=$PATH:/usr/local/go/bin # Add Golang's go and gofmt to path
 
 command -v yarn >/dev/null 2>&1 && { ( export PATH=$PATH:`yarn global bin` ) }
--- system_setup.sh
+++ system_setup.sh
@@ -1,57 +1,107 @@
 # Install my favourite programs on a new and shiny system
 
-echo "Installing command line tools..."
-
-# Transfer data with URL syntax
-command -v curl >/dev/null 2>&1 && echo "curl has already been installed on this system." || { sudo apt install curl; }
-
-# Version control
-command -v git >/dev/null 2>&1 && echo "git has already been installed on this system." || { sudo apt install git; }
-
-# Interactive processes viewer
-command -v htop >/dev/null 2>&1 && echo "htop has already been installed on this system." || { sudo apt install htop; }
-
-# Text editor
-command -v vim >/dev/null 2>&1 && echo "vim has already been installed on this system." || { sudo apt install vim; }
-
-# Terminal multiplexer
-command -v tmux >/dev/null 2>&1 && echo "tmux has already been installed on this system." || { sudo apt install tmux; }
-
-# Silver searcher
-command -v ag >/dev/null 2>&1 && echo "ag has already been installed on this system." || { sudo apt install silversearcher-ag; }
-
-# Copy to clipboard from command line
-command -v xclip >/dev/null 2>&1 && echo "xclip has already been installed on this system." || { sudo apt install xclip; }
-
-# Node version manager
-[ -d "$NVM_DIR/.git" ] && echo "nvm has already been installed on this system." || { curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash; source ~/.bash_profile; nvm install stable; nvm use stable; }
-
-echo ""
-echo "Would you like to install visual stuffs? This includes heavy things like gnome, spotify, chromium and atom."
-read -p "Continue? [Y/n]:" OKGO
+read -p "Install command line tools? [Y/n]:" OKGO
 OKGO=${OKGO:-Y}
-if [[ $OKGO =~ ^[yY]|[yY][eE][sS]$ ]]
+if ! [[ $OKGO =~ ^[yY]|[yY][eE][sS]$ ]]
 then
-	echo "You were warned. Commencing shininess..."
-else
-	exit
+	echo "Skipping the command line tools."
+else 
+	echo "Installing command line tools..."
+
+	# Transfer data with URL syntax
+	command -v curl >/dev/null 2>&1 && echo "curl already exists." || { sudo apt install curl; }
+
+	# Version control
+	command -v git >/dev/null 2>&1 && echo "git already exists." || { sudo apt install git; }
+
+	# Interactive processes viewer
+	command -v htop >/dev/null 2>&1 && echo "htop already exists." || { sudo apt install htop; }
+
+	# Text editor
+	command -v vim >/dev/null 2>&1 && echo "vim already exists." || { sudo apt install vim; }
+
+	# Terminal multiplexer
+	command -v tmux >/dev/null 2>&1 && echo "tmux already exists." || { sudo apt install tmux; }
+
+	# Silver searcher
+	command -v ag >/dev/null 2>&1 && echo "ag already exists." || { sudo apt install silversearcher-ag; }
+
+	# Copy to clipboard from command line
+	command -v xclip >/dev/null 2>&1 && echo "xclip already exists." || { sudo apt install xclip; }
 fi
 
-# Browser
-command -v chromium >/dev/null 2>&1 && echo "chromium has already been installed on this system." || { sudo snap install chromium; }
+echo
+read -p "Install programming languages? [y/N]:" OKGO
+OKGO=${OKGO:-N}
+if ! [[ $OKGO =~ ^[yY]|[yY][eE][sS]$ ]]
+then
+	echo "Skipping the programming languages."
+else 
+	echo "Installing programming languages..."
 
-# Integrated development environment (IDE)
-command -v atom >/dev/null 2>&1 && echo "atom has already been installed on this system." || { wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add - && sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list' && sudo apt update && sudo apt install atom && apm install sync-settings; }
+	# Node version manager
+	[ -d "$NVM_DIR/.git" ] && echo "nvm already exists." || {
+		read -p "Install nvm? [Y/n]:" OKGO
+		OKGO=${OKGO:-Y}
+		if [[ $OKGO =~ ^[yY]|[yY][eE][sS]$ ]]
+		then
+			curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash;
+			source ~/.bash_profile;
+			nvm install stable;
+			nvm use stable;
+		fi
+	}
 
-# Music player
-command -v spotify >/dev/null 2>&1 && echo "spotify has already been installed on this system." || { sudo snap install spotify; }
+	# Golang
+	command -v go >/dev/null 2>&1 && echo "go already exists." || {
+		read -p "Install go? [Y/n]:" OKGO
+		OKGO=${OKGO:-Y}
+		if [[ $OKGO =~ ^[yY]|[yY][eE][sS]$ ]]
+		then
+			GO_VERSION=1.19.1
+			GO_PLATFORM=linux-amd64
+			GO_FILENAME="go${GO_VERSION}.${GO_PLATFORM}.tar.gz"
+			GO_DOWNLOAD_LINK="https://go.dev/dl/${GO_FILENAME}"
 
-# Desktop manager
-command -v gnome-session >/dev/null 2>&1 && echo "gnome-session has already been installed on this system." || { sudo apt install gnome-session; }
+			curl -O -L $GO_DOWNLOAD_LINK
+			sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.19.1.linux-amd64.tar.gz
+			[[ -d /usr/local/go/bin ]] && export PATH=$PATH:/usr/local/go/bin # Also in .bash_profile
+			rm $GO_FILENAME
+			go version
+		fi
+	}
+fi
 
-# Gnome Tweak Tools
-command -v gnome-tweaks >/dev/null 2>&1 && echo "gnome-tweaks has already been installed on this system." || { sudo apt install gnome-tweaks; }
+echo
+read -p "Install many visual programs? [y/N]:" OKGO
+OKGO=${OKGO:-N}
+if ! [[ $OKGO =~ ^[yY]|[yY][eE][sS]$ ]]
+then
+	echo "Skipping the visual programs."
+else
+	echo "Installing visual programs..."
+	# Browsers
+	command -v chromium >/dev/null 2>&1 && echo "chromium already exists." || { sudo snap install chromium; }
+	command -v brave >/dev/null 2>&1 && echo "brave already exists." || { sudo snap install brave; }
 
+	# Editors
+	command -v atom >/dev/null 2>&1 && echo "atom already exists." || { wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add - && sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list' && sudo apt update && sudo apt install atom && apm install sync-settings; }
+	command -v code >/dev/null 2>&1 && echo "code already exists." || { sudo snap install code; }
+
+	# Music player
+	command -v spotify >/dev/null 2>&1 && echo "spotify already exists." || { sudo snap install spotify; }
+
+	# Gnome things
+	command -v gnome-session >/dev/null 2>&1 && echo "gnome-session already exists." || { sudo apt install gnome-session; }
+	command -v gnome-tweaks >/dev/null 2>&1 && echo "gnome-tweaks already exists." || { sudo apt install gnome-tweaks; }
+	command -v extension-manager >/dev/null 2>&1 && echo "extension-manager already exists." || { sudo apt install gnome-shell-extension-manager; }
+
+	# Selfies
+	command -v cheese >/dev/null 2>&1 && echo "cheese already exists." || { sudo snap install cheese --candidate; }
+fi
+
+
+echo
 # Enable dark mode
 echo "Dark mode! Enabling the Adwaita-dark theme"
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
