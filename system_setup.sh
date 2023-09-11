7d2ed7b891b2ff426415e37553ae434c6b651c4c
misund <thomas@hemmeligadresse.com>
2023-09-11 11:53:08 +0200

    feat(system setup): remove atom :'-(

--- system_setup.sh
+++ system_setup.sh
@@ -88,7 +88,6 @@ else
 	command -v brave >/dev/null 2>&1 && echo "brave already exists." || { sudo snap install brave; }
 
 	# Editors
-	command -v atom >/dev/null 2>&1 && echo "atom already exists." || { wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add - && sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list' && sudo apt update && sudo apt install atom && apm install sync-settings; }
 	command -v code >/dev/null 2>&1 && echo "code already exists." || { sudo snap install --classic code; }
 
 	# Communication
